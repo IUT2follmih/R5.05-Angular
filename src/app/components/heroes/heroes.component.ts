a1313c54088366559456de37333c5b01bd44bfdd
LeeeeePoulpe <hfollmi@gmail.com>
2024-09-23 09:22:32 +0200

    TP1 step one

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {HeroInterface} from "./heroInterface";
+import {UpperCasePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [
+    UpperCasePipe,
+    FormsModule
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-
+  hero: HeroInterface = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
