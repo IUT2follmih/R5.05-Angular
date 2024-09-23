50cebade029630804dda55b5833f6bfda1153eb3
LeeeeePoulpe <hfollmi@gmail.com>
2024-09-23 09:36:18 +0200

    Step 2 part one

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {HeroInterface} from "../../data/heroInterface";
 import {UpperCasePipe} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {HEROES} from "../../data/mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -14,6 +15,8 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+  heroes = HEROES;
+
   hero: HeroInterface = {
     id: 1,
     name: 'Windstorm'
---
+++ src/app/data/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { HeroInterface } from './heroInterface';
+export const HEROES: HeroInterface[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+];
