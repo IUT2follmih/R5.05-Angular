6f6ff2266c0f4da6847d3bd46a132d63d08dcd61
LeeeeePoulpe <hfollmi@gmail.com>
2024-09-25 09:00:24 +0200

    Add a fake call to service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HeroInterface} from "../data/heroInterface";
 import { HEROES } from "../data/mock-heroes";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,14 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): HeroInterface[] {
-    return HEROES;
+  getHeroes(): Observable<HeroInterface[]> {
+    const heroes = of(HEROES);
+    return heroes;
+    /*const heroes: Observable<HeroInterface[]> = new Observable(observer => {
+      observer.next(HEROES.slice(0,2)); // 2 éléments envoyés
+      setTimeout(() => {
+        observer.next(HEROES); observer.complete();
+      }, 3000); });
+    return heroes;*/
   }
 }
