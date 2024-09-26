5c7535960137817f0b5347a6e43cae224999572d
LeeeeePoulpe <hfollmi@gmail.com>
2024-09-26 14:16:46 +0200

    Adding css dans nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {HeroesComponent} from "./components/heroes/heroes.component";
 import {MessagesComponent} from "./components/messages/messages.component";
+import {NavbarComponent} from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
